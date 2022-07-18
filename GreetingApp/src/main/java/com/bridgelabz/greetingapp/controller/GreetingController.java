d735c6ca6c2a507ac938457da01999c670bcc7e3
nehachougule1998 <nchougule734@gmail.com>
2022-07-19 00:41:41 +0530

    Greeting With Service

--- GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -1,5 +1,7 @@
 package com.bridgelabz.greetingapp.controller;
 
+import com.bridgelabz.greetingapp.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -7,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
-
+    @Autowired
+    GreetingService greetingService;
     @GetMapping("/getMap")
     public String getmessage(){
         return "Hello World";
@@ -19,6 +22,12 @@ public class GreetingController {
     @GetMapping("/getpath/{name}")
     public String getpath(@PathVariable String name){
        return " Hello " +name;
+    }
+    //UC2
+    @GetMapping("/getbyservice")
+    public String getbyservice(){
+        String message=greetingService.getmessage();
+        return message;
     }
 
 }
