32aa6bdaee147f5207a9f69c1c249e1a6d6c88a9
nehachougule1998 <nchougule734@gmail.com>
2022-07-18 23:44:05 +0530

    UsingGreetingController

---
+++ GreetingApp/src/main/java/com/bridgelabz/greetingapp/controller/GreetingController.java
@@ -0,0 +1,24 @@
+package com.bridgelabz.greetingapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    @GetMapping("/getMap")
+    public String getmessage(){
+        return "Hello World";
+    }
+    @GetMapping("/getbyparam")
+    public String getByParam(@RequestParam String name){
+        return " Hello " +name;
+    }
+    @GetMapping("/getpath/{name}")
+    public String getpath(@PathVariable String name){
+       return " Hello " +name;
+    }
+
+}
